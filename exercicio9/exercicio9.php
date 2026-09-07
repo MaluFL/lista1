76f4336136959298bfb1b294e3797d9493ab67b9
MaluFL <mluizaf.fatec@gmail.com>
2026-09-07 20:03:35 -0300

    implementação do exercício 9

---
+++ exercicio9/exercicio9.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <h1>Exercício 9 - Perímetro do Retângulo</h1>
+    <form method="post" action="resposta9.php">
+        <label for="largura">Informe a largura do retângulo: </label>
+        <input type="text" id="largura" name="largura">
+        <p>
+        <label for="altura">Informe a altura do retângulo: </label>
+        <input type="text" id="altura" name="altura">
+        <p>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exercicio9/resposta9.php
@@ -0,0 +1,5 @@
+<?php
+    $largura = $_POST["largura"];
+    $altura = $_POST["altura"];
+    $perimetro = 2 * ($largura + $altura);
+    echo "O perímetro do retângulo é: $perimetro";
