d4ecbddd4e35e9fbea0bcabd09c056dccc7b7a60
MaluFL <mluizaf.fatec@gmail.com>
2026-09-07 19:05:36 -0300

    implementação dos 4 primeiros exercícios

---
+++ exercicio4/exercicio4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 4</title>
+</head>
+<body>
+    <h1>Exercício 4 - MÉDIA</h1>
+    <form method="post" action="resposta4.php">
+        <label for="valor1">Informe o primeiro valor:</label>
+        <input type="number" id="valor1" name="valor1">
+        <p>
+        <label for="valor2">Informe o segundo valor:</label>
+        <input type="number" id="valor2" name="valor2">
+        <p>
+        <label for=valor3>Informe o terceiro valor: </label>
+        <input type="number" id="valor3" name="valor3">
+        <p>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
