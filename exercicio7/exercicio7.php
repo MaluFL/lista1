10b8567d9d3044965700d977d8785a0224edced3
MaluFL <mluizaf.fatec@gmail.com>
2026-09-07 19:40:33 -0300

    implementação do exercício 7

---
+++ exercicio7/exercicio7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7</title>
+</head>
+<body>
+    <h1>Exercício 7 - Área do Retângulo</h1>
+    <form method="post" action="resposta7.php">
+        <label for="largura">Informe a largura do retângulo: </label>
+        <input type="text" id="largura" name="largura">
+        <p>
+        <label for="altura">Informe a altura do retângulo: </label>
+        <input type="text" id="altura" name="altura">
+        <p>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exercicio7/resposta7.php
@@ -0,0 +1,5 @@
+<?php
+    $largura = $_POST["largura"];
+    $altura = $_POST["altura"];
+    $area = $largura * $altura;
+    echo "A área do retângulo é: $area";
