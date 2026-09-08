168214bfe4a9d2dd5721e85ea151f6342bbe7a55
MaluFL <mluizaf.fatec@gmail.com>
2026-09-08 19:26:51 -0300

    implementação do exercício 19

---
+++ exercicio19/exercicio19.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 19</title>
+</head>
+<body>
+    <h1>Exercício 19 - Velocidade Média</h1>
+    <form method="post" action="resposta19.php">
+        <label for="distancia">Informe a distância (km): </label>
+        <input type="text" id="distancia" name="distancia">
+        <p>
+        <label for="tempo">Informe o tempo (horas): </label>
+        <input type="text" id="tempo" name="tempo">
+        <p>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exercicio19/resposta19.php
@@ -0,0 +1,5 @@
+<?php
+    $distancia = $_POST["distancia"];
+    $tempo = $_POST["tempo"];
+    $velocidade = $distancia / $tempo;
+    echo "A velocidade média é: $velocidade km/h";
