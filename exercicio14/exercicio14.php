1b61089996fa5b664322618cbb49339bf663b3b1
MaluFL <mluizaf.fatec@gmail.com>
2026-09-07 21:44:34 -0300

    implementação do exercício 14

---
+++ exercicio14/exercicio14.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 14</title>
+</head>
+<body>
+    <h1>Exercício 14 - IMC</h1>
+    <form method="post" action="resposta14.php">
+        <label for="peso">Informe o peso (kg): </label>
+        <input type="text" id="peso" name="peso">
+        <p>
+        <label for="altura">Informe a altura (m): </label>
+        <input type="text" id="altura" name="altura">
+        <p>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exercicio14/resposta14.php
@@ -0,0 +1,5 @@
+<?php
+    $peso = $_POST["peso"];
+    $altura = $_POST["altura"];
+    $imc = $peso / ($altura * $altura);
+    echo "O IMC é: $imc";
